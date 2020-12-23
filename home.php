fc80bd7a6d0ddeb9bb06a2ea3bd48581a4644c1a
Alankrit Vyas <66865682+alankritvyas@users.noreply.github.com>
2020-12-23 12:25:56 +0530

    Add files via upload

---
+++ home.php
@@ -0,0 +1,24 @@
+<?php 
+session_start();
+
+if (isset($_SESSION['id']) && isset($_SESSION['user_name'])) {
+
+ ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>HOME</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<a href="index2.php">My portfolio website</a>
+     <a href="logout.php">Logout</a>
+</body>
+</html>
+
+<?php 
+}else{
+     header("Location: index.php");
+     exit();
+}
+ ?>
